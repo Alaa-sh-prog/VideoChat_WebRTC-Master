104f687872130281761679471d62b2e8675c12d4
Alaa-sh-prog <alaa93alshofi@gmail.com>
2020-06-14 16:18:42 +0400

    forst commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + "/public"))
+let clients = 0
+
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('CreatePeer')
+            }
+        }
+        else
+            this.emit('SessionActive')
+        clients++;
+    })
+    socket.on('Offer', SendOffer)
+    socket.on('Answer', SendAnswer)
+    socket.on('disconnect', Disconnect)
+})
+
+function Disconnect() {
+    if (clients > 0) {
+        if (clients <= 2)
+            this.broadcast.emit("Disconnect")
+        clients--
+    }
+}
+
+function SendOffer(offer) {
+    this.broadcast.emit("BackOffer", offer)
+}
+
+function SendAnswer(data) {
+    this.broadcast.emit("BackAnswer", data)
+}
+
+http.listen(port, () => console.log(`Active on ${port} port`))
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+// //   res.writeHead(200, {'Content-Type': 'text/plain'});
+// //   res.end('Hello World\n');
+// }).listen(8124, "127.0.0.1");
+// console.log('Server running at http://127.0.0.1:8124/');
+
+
+
